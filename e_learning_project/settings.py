9ae21f2109f47421a6ad68fd2f96ba5699868797
Sumit Sidharth <sumitsidharth30@gmail.com>
2026-05-14 22:11:35 +0530

    Update ALLOWED_HOSTS to wildcard for easier deployment

--- e_learning_project/settings.py
+++ e_learning_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY', default='django-insecure-v8#e$cxxc!+tr0kzak543kwt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=True)
 
-ALLOWED_HOSTS = ['sumitelearning.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
